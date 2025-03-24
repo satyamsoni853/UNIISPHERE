b4c1b4cb057b619306fa69ae13d00e888347db4e
Sachin Pal <sachinpalsp1402@gmail.com>
2025-03-24 23:17:42 +0530

    add the comment and the share section in the mobile view as well as responsive nes

--- src/Componets/DesktopMiddle/DesktopMiddle.jsx
+++ src/Componets/DesktopMiddle/DesktopMiddle.jsx
@@ -18,6 +18,15 @@ import ShareIcon from "./Share.svg";
 import LikeIcon from "./Like.svg";
 import CommentIcon from "./Comment.svg";
 
+// =========Share box data =============
+
+import savedIcon from "./saved.svg";
+import whatsappIcon from "./Whatsapp.svg";
+import facebookIcon from "./Facebook.svg";
+import instaIcon from "./insta.svg";
+import linkIcon from "./Link.svg";
+import xIcon from "./X.svg";
+
 function DesktopMiddle() {
   // Full comment box data
 
@@ -61,7 +70,69 @@ function DesktopMiddle() {
     },
   ];
   const [showComment, setShowComment] = useState(false);
-  //
+  //  ===============SHARE BOX DATA  ==================
+
+  const persons = [
+    {
+      name: "Anjali",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Rohit",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Anjali",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Rohit",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Anjali",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Rohit",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Anjali",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Rohit",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Anjali",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Rohit",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Anjali",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Rohit",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Anjali",
+      avatar: profilePhoto,
+    },
+    {
+      name: "Rohit",
+      avatar: profilePhoto,
+    },
+  ];
+
+  const [showShare, setShowshare] = useState(false);
+
   const [posts, setPosts] = useState([]);
   const [imageLoading, setImageLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -258,7 +329,8 @@ function DesktopMiddle() {
                     onClick={() => handleLike(index)}
                   >
                     <span className="middle-icon-count">{post.likes}</span>
-                    <CiHeart
+                    <img
+                      src={LikeIcon}
                       className={`middle-icon ${post.isLiked ? "liked" : ""}`}
                     />
                   </div>
@@ -274,8 +346,13 @@ function DesktopMiddle() {
                     </span>
                     <img src={Commenticonsvg} alt="Comment" />
                   </div>
-                  <div className="middle-icon-container">
-                    <PiShareFatThin className="middle-icon" />
+                  <div
+                    onClick={() => {
+                      setShowshare(true);
+                    }}
+                    className="middle-icon-container"
+                  >
+                    <img src={ShareIcon} className="middle-icon" />
                   </div>
                 </div>
               </div>
@@ -292,20 +369,17 @@ function DesktopMiddle() {
         ) : (
           <p>No posts available</p>
         )}
-
-        
       </div>
 
-      {/* ## *******************Comment Box CODE ******************##  */}
+      {/* ## *******************COMMENT BOX CODE *********************##  */}
 
       {showComment && (
         <div className="Full-comment-section-main-container">
           {/* Left Section */}
           <div className="Full-comment-section-left-section">
             <div className="Full-comment-section-user-profile-header">
-               
               <img
-                src={userData.profilePicture  }
+                src={userData.profilePicture}
                 alt="Profile"
                 className="Full-comment-section-profile-picture"
               />
@@ -431,6 +505,122 @@ function DesktopMiddle() {
                 setShowComment((prev) => !prev);
               }}
               className="Full-comment-section-cross-button"
+            >
+              {" "}
+              ×
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* ## ===================SHARE BOX   ======================## */}
+
+      {showShare && (
+        <div className="Full-share-section-main-container">
+          {/* Left Section */}
+          <div className="Full-share-section-left-section">
+            <div className="Full-share-section-user-profile-header">
+              <div className="Full-share-section-top-image-and-names">
+                <img
+                  src={userData.profilePicture}
+                  alt="Profile"
+                  className="Full-share-section-profile-picture"
+                />
+                <div className="Full-share-section-user-info">
+                  <div className="Full-share-section-name-and-postTime">
+                    <span className="Full-share-section-user-name">
+                      {userData.name}
+                    </span>
+                    <span className="Full-share-section-user-details">18h</span>
+                  </div>
+                  <div className="Full-share-section-work-and-education">
+                    <span className="Full-share-section-user-details">
+                      {userData.education}
+                    </span>
+                    <span className="Full-share-section-user-details">||</span>
+                    <span className="Full-share-section-user-details">
+                      {" "}
+                      {userData.workPlace}{" "}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <img
+                src={Threedot}
+                className="Full-share-section-menu-icon"
+                alt=""
+              />
+            </div>
+            <div className="Full-share-section-photo-container">
+              <img
+                src={userData.profilePicture}
+                alt="Post"
+                className="Full-share-section-post-photo"
+              />
+              <div className="Full-share-section-action-buttons">
+                <div className="Full-share-section-connect-div">
+                  <img
+                    src={Connect}
+                    className="Full-share-section-connect-icon"
+                    alt=""
+                  />
+                </div>
+                <div className="Full-share-section-share-like-share-icon">
+                  <img
+                    src={ShareIcon}
+                    className="Full-share-section-post-icons"
+                    alt=""
+                  />
+                  <img
+                    src={CommentIcon}
+                    className="Full-share-section-post-icons"
+                    alt=""
+                  />
+                  <img
+                    src={LikeIcon}
+                    className="Full-share-section-post-icons"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Right Section */}
+          <div className="Full-share-section-right-section">
+            <h1 className="Full-share-section-heading">Share</h1>
+            <div className="Full-share-section-innerDiv">
+              <div className="Full-share-section-AvtaarAndName-collection">
+                {persons.map((val) => (
+                  <div className="Full-share-section-AvtaarAndName">
+                    <img src={val.avatar} alt="" />
+                    <h1>{val.name}</h1>
+                  </div>
+                ))}
+              </div>
+              <div className="Full-share-section-AllIcons">
+                <img src={savedIcon} alt="" />
+                <img src={linkIcon} alt="" />
+                <img src={xIcon} alt="" />
+                <img src={whatsappIcon} alt="" />
+                <img src={facebookIcon} alt="" />
+                <img src={instaIcon} alt="" />
+              </div>
+            </div>
+
+            <div className="Full-share-section-share-input-and-image">
+              <img
+                src={profilePhoto}
+                className="Full-share-section-sharePerson-image"
+                alt=""
+              />
+              <input type="text" placeholder="Write a share to VIJAY PRASAD" />
+            </div>
+            <button
+              onClick={() => {
+                setShowshare(false);
+              }}
+              className="Full-share-section-cross-button"
             >
               {" "}
               ×
