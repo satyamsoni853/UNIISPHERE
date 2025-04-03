262db6454877c543d186f486411ca2ac2461a7ca
Sachin Pal <sachinpalsp1402@gmail.com>
2025-04-03 22:14:53 +0530

    left section , share section , and the cross button done

--- src/Componets/DesktopLeftbottom/DesktopLeftbottom.jsx
+++ src/Componets/DesktopLeftbottom/DesktopLeftbottom.jsx
@@ -1,43 +1,36 @@
-import React from "react"; 
+import React, { useState } from "react";
 import "./DesktopLeftbottom.css";
-import DesktopLeft1image1 from "./DesktopLeft1image1.png";
-import DesktopLeft1image2 from "./DesktopLeft1image2.png";
-import DesktopLeft1image3 from "./DesktopLeft1image3.png";
+import image1 from "./image1.png";
+import image2 from "./image2.png";
+import image3 from "./image3.png";
 
 function DesktopLeftbottom() {
+  const [suggestedItems, setSuggestedItems] = useState([
+    { id: 1, topic: "Physics: Sem 2", author: "R.S Aggarwal", image: image1 },
+    { id: 2, topic: "The SAGA", author: "Yash Sharma", image: image2 },
+    { id: 3, topic: "Maths: Sem 5", author: "Oswal Production", image: image3 },
+  ]);
   return (
     <div className="leftsectionbottom-1">
-      <h3 className="leftsectionbottom-1-heading">Upcoming Events</h3>
+      <h3 className="leftsectionbottom-1-heading">Suggested</h3>
 
-      {/* Event 1 */}
-      <div className="leftsectionbottom-1-event">
-        <img src={DesktopLeft1image1} alt="Event 1" className="leftsectionbottom-1-image" />
-        <div className="leftsectionbottom-1-details">
-          <p className="leftsectionbottom-1-date">26 Jan 2025</p>
-          <p className="leftsectionbottom-1-location">University of Delhi</p>
-        </div>
-        <a href="#" className="leftsectionbottom-1-link">Read More</a>
-      </div>
-
-      {/* Event 2 */}
-      <div className="leftsectionbottom-1-event">
-        <img src={DesktopLeft1image2} alt="Event 2" className="leftsectionbottom-1-image" />
-        <div className="leftsectionbottom-1-details">
-          <p className="leftsectionbottom-1-date">26 Jan 2025</p>
-          <p className="leftsectionbottom-1-location">University of Delhi</p>
-        </div>
-        <a href="#" className="leftsectionbottom-1-link">Read More</a>
-      </div>
-
-      {/* Event 3 */}
-      <div className="leftsectionbottom-1-event">
-        <img src={DesktopLeft1image3} alt="Event 3" className="leftsectionbottom-1-image" />
-        <div className="leftsectionbottom-1-details">
-          <p className="leftsectionbottom-1-date">26 Jan 2025</p>
-          <p className="leftsectionbottom-1-location">University of Delhi</p>
-        </div>
-        <a href="#" className="leftsectionbottom-1-link">Read More</a>
-      </div>
+      {/* Events  */}
+      {suggestedItems.map((val) => (
+        <div className="leftsectionbottom-1-event">
+          <img
+            src={val.image}
+            alt="Event 1"
+            className="leftsectionbottom-1-image"
+          />
+          <div className="leftsectionbottom-1-details">
+            <p className="leftsectionbottom-1-date">{val.topic}</p>
+            <p className="leftsectionbottom-1-location">{val.author}</p>
+          </div>
+          <a href="#" className="leftsectionbottom-1-link">
+            Read More
+          </a>
+        </div> 
+      ))}
 
       <p className="leftsectionbottom-1-button">See All</p>
     </div>
