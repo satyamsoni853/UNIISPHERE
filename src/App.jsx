2a4819fcd7aeabd9c9cedf82f69579d7cb563f57
satyam soni <satyamsoni853@gmail.com>
2025-04-06 01:16:22 +0530

    Done111

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 import UserLogin from './Componets/UserLogin/UserLogin.jsx'
 import AfterOtpSection1 from "./Componets/AfterOtpSection1/AfterOtpSection1.jsx";
