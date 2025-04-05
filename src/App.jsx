85548fe94c6d2e48799f1cb146b8c51070a0290c
Sachin Pal <sachinpalsp1402@gmail.com>
2025-04-05 22:20:35 +0530

    done mobile settings

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import NetworkPage from "./Componets/NetworkPage/NetworkPage.jsx";
 import CollabPage from "./Componets/CollabPage/CollabPage.jsx";
 import BottomMessagesWidget from "./Componets/BottomMessagesWidget/BottomMessagesWidget.jsx";
 import UploadSection from "./Componets/UploadSection/UploadSection.jsx";
- 
+
 
 
 function App() {
@@ -76,7 +76,6 @@ function App() {
             element={<BottomMessagesWidget />}
           />
           <Route path="/uploadsection/:userId" element={<UploadSection />} />
-        
           
         </Routes>
       </div>
