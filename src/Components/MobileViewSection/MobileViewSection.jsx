dbdca3bc9f532484e2c3d9c59557c69143d28c5c
satyam soni <satyamsoni853@gmail.com>
2025-04-22 17:13:23 +0530

    done221

--- src/Components/MobileViewSection/MobileViewSection.jsx
+++ src/Components/MobileViewSection/MobileViewSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Background from '../Background/Background';
-import MobileFooter from '../MobileFooter/MobileFooter'
+import MobileFooter from '../MobileFooter/MobileFooter.jsx'
 import MobileNavbar from '../MobileNavbar/MobileNavbar';
 import MobileMiddleSection from '../MobileMiddleSection/MobileMiddleSection'; // Removed .jsx
 import './MobileViewSection.css'; // Corrected CSS file name for consistency
