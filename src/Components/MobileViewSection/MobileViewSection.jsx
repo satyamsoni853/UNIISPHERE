893b9958aeaa49d62a6b9f351a7eb3bc7ccfa9a4
satyam soni <satyamsoni853@gmail.com>
2025-04-22 18:37:17 +0530

    done229

--- src/Components/MobileViewSection/MobileViewSection.jsx
+++ src/Components/MobileViewSection/MobileViewSection.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Background from '../Background/Background.jsx'; // Added .jsx for consistency
 import MobileNavbar from '../MobileNavbar/MobileNavbar.jsx'; // Added .jsx for consistency
-import MobileMiddleSection from '../MobileMiddleSection/MobileMiddleSection.jsx'
-import MobileFooter from '../MobileFooter.jsx'; // Corrected path assuming file is in src/Components/
+import MobileMiddleSection from '../MobileMiddleSection/MobileMiddleSection.'
+import MobileFooter from "../MobileFooter/MobileFooter.jsx"
 import './MobileViewSection.css';
 
 function MobileViewSection() {
